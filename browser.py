1b3497a1017f842fabf3ad683daf885abcd2ddac
Abhinav Paudel <41516895+Abhi1997@users.noreply.github.com>
2025-11-29 20:29:27 +0545

    Just importing all possible requirements

--- browser.py
+++ browser.py
@@ -1,2 +1,9 @@
 import sys
-from PyQt5.QtCore import QUrl
+from PyQt6.QtCore import QUrl, QSize, Qt
+from PyQt6.QtWidgets import (
+    QMainWindow, QWidget, QVBoxLayout, QPushButton, QLineEdit, 
+    QTabWidget, QApplication, QToolBar, QStatusBar, QComboBox, QMessageBox
+)
+from PyQt6.QtGui import QAction, QIcon
+from PyQt6.QtWebEngineWidgets import QWebEngineView
+from PyQt6.QtWebEngineCore import QWebEnginePage, QWebEngineProfile
